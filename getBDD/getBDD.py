dec707e71421d977268e903f424889bf210755ac
Mohamed-elg <mohamed.elguermat9@gmail.com>
2023-06-15 16:56:45 +0200

    update getBDD

--- getBDD/getBDD.py
+++ getBDD/getBDD.py
@@ -4,9 +4,8 @@ print("downloading...")
 rdf_url = "https://diffuseur.datatourisme.fr/webservice/37bc3e10f044c1d393d523b99482ad3c/bb3e487f-7b9f-4085-b409-a530cbe2fb90"
 rdf_data = requests.get(rdf_url).content
 
-
 print("sending...")
-blazegraph_url = "http://localhost:9999/blazegraph/namespace/kb/sparql"
+blazegraph_url = "http://127.0.0.1:9999/blazegraph/namespace/kb/sparql"
 headers = {"Content-Type": "application/rdf+xml"}
 response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
 
