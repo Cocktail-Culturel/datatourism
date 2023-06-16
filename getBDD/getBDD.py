27d1c944fa9445bfdb7f44f7e88a0c92520f0435
Mohamed-elg <mohamed.elguermat9@gmail.com>
2023-06-16 11:08:00 +0200

    nouveau flux

--- getBDD/getBDD.py
+++ getBDD/getBDD.py
@@ -1,11 +1,11 @@
 import requests
 
 print("downloading...")
-rdf_url = "https://diffuseur.datatourisme.fr/webservice/37bc3e10f044c1d393d523b99482ad3c/bb3e487f-7b9f-4085-b409-a530cbe2fb90"
+rdf_url = "https://diffuseur.datatourisme.fr/webservice/f29b9452652d02cd4a81bad33b5cf1d0/bb3e487f-7b9f-4085-b409-a530cbe2fb90"
 rdf_data = requests.get(rdf_url).content
 
 print("sending...")
-blazegraph_url = "http://127.0.0.1:9999/blazegraph/namespace/kb/sparql"
+blazegraph_url = "http://localhost:9999/blazegraph/namespace/kb/sparql"
 headers = {"Content-Type": "application/rdf+xml"}
 response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
 
