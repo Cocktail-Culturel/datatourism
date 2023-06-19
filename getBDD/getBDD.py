7961abb06088be6d0b8a92d2a64d6ef68910d9fd
BIER <cyril.bier42100@gmail.com>
2023-06-19 15:02:15 +0200

    ajout try-except dans getBDD.py

--- getBDD/getBDD.py
+++ getBDD/getBDD.py
@@ -7,13 +7,17 @@ with open('flux.json') as file:
 for region, url in json_data.items():
     print("downloading...")
     url += "bb3e487f-7b9f-4085-b409-a530cbe2fb90"
-    print(url)
-    rdf_data = requests.get(url).content
+    try:
+        rdf_data = requests.get(url).content
+        print(f"{region} flux downloaded")
+    except requests.exceptions.RequestException as err:
+        print(f"Error : {err}")
 
     print("sending...")
     blazegraph_url = "http://localhost:9999/blazegraph/namespace/kb/sparql"
     headers = {"Content-Type": "application/rdf+xml"}
-    response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
-
-
-print("Response Status Code:", response.status_code)
+    try:
+        response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
+        print("Response Status Code:", response.status_code)
+    except requests.exceptions.RequestException as err:
+        print(f"Error : {err} Staus code : {response.status_code}")
