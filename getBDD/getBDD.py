e251205d3cf1283cca373ad18ed50c7c9087eee8
BIER <cyril.bier42100@gmail.com>
2023-06-19 16:30:36 +0200

    Format heure logs

--- getBDD/getBDD.py
+++ getBDD/getBDD.py
@@ -9,23 +9,25 @@ with open('flux.json') as file:
     json_data = json.load(file)
 
 for region, url in json_data.items():
-    log_file.write(f"[{datetime.now()}] Downloading {region} flux\n")
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_file.write(f"[{current_time}] Downloading {region} flux\n")
     url += "bb3e487f-7b9f-4085-b409-a530cbe2fb90"
     try:
         rdf_data = requests.get(url).content
-        log_file.write(f"[{datetime.now()}] {region} flux downloaded\n")
+        log_file.write(f"[{current_time}] {region} flux downloaded\n")
     except requests.exceptions.RequestException as err:
-        log_file.write(f"[{datetime.now()}] Error: {err}\n")
+        log_file.write(f"[{current_time}] Error: {err}\n")
 
     sleep(3)
-    log_file.write(f"[{datetime.now()}] Loading {region} in the database\n")
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_file.write(f"[{current_time}] Loading {region} in the database\n")
     blazegraph_url = "http://localhost:9999/blazegraph/namespace/kb/sparql"
     headers = {"Content-Type": "application/rdf+xml"}
     try:
         response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
-        log_file.write(f"[{datetime.now()}] Load finished with status code: {response.status_code}\n")
+        log_file.write(f"[{current_time}] Load finished with status code: {response.status_code}\n")
     except requests.exceptions.RequestException as err:
-        log_file.write(f"[{datetime.now()}] Error: {err} Status code: {response.status_code}\n")
+        log_file.write(f"[{current_time}] Error: {err} Status code: {response.status_code}\n")
 
     sleep(2)
 
