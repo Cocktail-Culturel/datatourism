1cbfd8541e0dcabc514da06dfa0899b4c09d4be2
BIER <cyril.bier42100@gmail.com>
2023-06-19 10:38:04 +0200

    Modification getBDD pour complèter entièrement BDD

--- getBDD/getBDD.py
+++ getBDD/getBDD.py
@@ -1,13 +1,17 @@
 import requests
+import json
 
-print("downloading...")
-rdf_url = "https://diffuseur.datatourisme.fr/webservice/f29b9452652d02cd4a81bad33b5cf1d0/bb3e487f-7b9f-4085-b409-a530cbe2fb90"
-rdf_data = requests.get(rdf_url).content
+with open('flux.json') as file:
+    json_data = json.load(file)
+    
+for region, url in json_data.items():
+    print("downloading...")
+    rdf_data = requests.get(url).content
 
-print("sending...")
-blazegraph_url = "http://localhost:9999/blazegraph/namespace/kb/sparql"
-headers = {"Content-Type": "application/rdf+xml"}
-response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
+    print("sending...")
+    blazegraph_url = "http://localhost:9999/blazegraph/namespace/kb/sparql"
+    headers = {"Content-Type": "application/rdf+xml"}
+    response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
 
 
 print("Response Status Code:", response.status_code)
