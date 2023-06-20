91ce560927d746931155e3a19c5a783368873c62
BIER <cyril.bier42100@gmail.com>
2023-06-20 11:22:11 +0200

    modif getBDD chargement tout les flux

--- getBDD/getBDD.py
+++ getBDD/getBDD.py
@@ -1,49 +1,55 @@
-# from time import sleep
-# import requests
-# import json
-# from datetime import datetime
-
-# log_file = open('log.txt', 'a')
-
-# with open('flux.json') as file:
-#     json_data = json.load(file)
-
-# for region, url in json_data.items():
-#     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     log_file.write(f"[{current_time}] Downloading {region} flux\n")
-#     url += "bb3e487f-7b9f-4085-b409-a530cbe2fb90"
-#     try:
-#         rdf_data = requests.get(url).content
-#         log_file.write(f"[{current_time}] {region} flux downloaded\n")
-#     except requests.exceptions.RequestException as err:
-#         log_file.write(f"[{current_time}] Error: {err}\n")
-
-#     sleep(3)
-#     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     log_file.write(f"[{current_time}] Loading {region} in the database\n")
-    
-#     try:
-#         blazegraph_url = "http://localhost:9999/blazegraph/namespace/kb/sparql"
-#         headers = {"Content-Type": "application/rdf+xml"}
-#         response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
-#         log_file.write(f"[{current_time}] Load finished with status code: {response.status_code}\n")
-#     except requests.exceptions.RequestException as err:
-#         log_file.write(f"[{current_time}] Error: {err} Status code: {response.status_code}\n")
-
-#     sleep(2)
-
-# log_file.close()
-
+from time import sleep
 import requests
+import json
+from datetime import datetime
 
-print("downloading...")
-rdf_url = "https://diffuseur.datatourisme.fr/webservice/07ede464e2d769db91fa9d7221a27c0a/bb3e487f-7b9f-4085-b409-a530cbe2fb90"
-rdf_data = requests.get(rdf_url).content
+#log_file = open('log.txt', 'a')
 
-print("sending...")
-blazegraph_url = "http://localhost:9999/blazegraph/namespace/kb/sparql"
-headers = {"Content-Type": "application/rdf+xml"}
-response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
+with open('flux.json') as file:
+    json_data = json.load(file)
+
+for region, url in json_data.items():
+    #current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    #log_file.write(f"[{current_time}] Downloading {region} flux\n")
+    print(f"Downloading {region}")
+    url += "bb3e487f-7b9f-4085-b409-a530cbe2fb90"
+    try:
+        rdf_data = requests.get(url).content
+        #log_file.write(f"[{current_time}] {region} flux downloaded\n")
+        print("Downloaded")
+    except requests.exceptions.RequestException as err:
+        #log_file.write(f"[{current_time}] Error: {err}\n")
+        print(err)
+
+    #sleep(3)
+    #current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    #log_file.write(f"[{current_time}] Loading {region} in the database\n")
+    print("Loading in DB")
+    
+    try:
+        blazegraph_url = "http://localhost:9999/blazegraph/namespace/kb/sparql"
+        headers = {"Content-Type": "application/rdf+xml"}
+        response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
+        #log_file.write(f"[{current_time}] Load finished with status code: {response.status_code}\n")
+        print("loaded")
+    except requests.exceptions.RequestException as err:
+        #log_file.write(f"[{current_time}] Error: {err} Status code: {response.status_code}\n")
+        print(err)
+
+    #sleep(2)
+
+#log_file.close()
+
+# import requests
+
+# print("downloading...")
+# rdf_url = "https://diffuseur.datatourisme.fr/webservice/f29b9452652d02cd4a81bad33b5cf1d0/bb3e487f-7b9f-4085-b409-a530cbe2fb90"
+# rdf_data = requests.get(rdf_url).content
+
+# print("sending...")
+# blazegraph_url = "http://localhost:9999/blazegraph/namespace/kb/sparql"
+# headers = {"Content-Type": "application/rdf+xml"}
+# response = requests.post(blazegraph_url, data=rdf_data, headers=headers)
 
 
-print("Response Status Code:", response.status_code)
+# print("Response Status Code:", response.status_code)
