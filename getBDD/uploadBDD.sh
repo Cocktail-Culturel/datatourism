bb53b0fac3ccea7e3777f76f9217e01b7ff0baf0
BIER <cyril.bier42100@gmail.com>
2023-06-21 14:38:54 +0200

    modif script pour passer l'url en paramètre

--- getBDD/uploadBDD.sh
+++ getBDD/uploadBDD.sh
@@ -27,30 +27,25 @@
 #     echo
 # done
 
-
-# Lecture du fichier flux.json
-data=$(cat flux.json)
-
-# Extraction de l'URL de la Nouvelle-Aquitaine
-url=$(echo "$data" | jq -r '."Nouvelle_Aquitaine"')
-
-# Vérification si l'URL de la Nouvelle-Aquitaine est définie
-if [[ -n $url ]]; then
-    modified_url="$url"bb3e487f-7b9f-4085-b409-a530cbe2fb90
-
-    echo "Téléchargement de $modified_url ..."
-    wget "$modified_url" -O flux.gz
-
-    echo "Décompression de flux.gz ..."
-    gzip -d flux.gz
-
-    echo "Renommage du fichier décompressé en flux.rdf ..."
-    mv flux flux.rdf
-
-    echo "Envoi de flux.rdf vers http://localhost:9999/blazegraph/namespace/kb/sparql ..."
-    curl -X POST -H "Content-Type:application/rdf+xml" --data-binary @flux.rdf "http://localhost:9999/blazegraph/namespace/kb/sparql"
-
-    echo "Traitement terminé pour l'URL de la Nouvelle-Aquitaine."
-else
-    echo "L'URL de la Nouvelle-Aquitaine n'est pas définie dans le fichier flux.json."
+# Vérification si l'URL est passée en tant que paramètre
+if [[ -z $1 ]]; then
+    echo "Veuillez fournir l'URL en tant que paramètre."
+    exit 1
 fi
+
+url=$1
+modified_url="$url"bb3e487f-7b9f-4085-b409-a530cbe2fb90
+
+echo "Téléchargement de $modified_url ..."
+wget "$modified_url" -O flux.gz
+
+echo "Décompression de flux.gz ..."
+gzip -d flux.gz
+
+echo "Renommage du fichier décompressé en flux.rdf ..."
+mv flux flux.rdf
+
+echo "Envoi de flux.rdf vers https://datatourism-bdd.cocktail-culturel.com/blazegraph/namespace/kb/sparql ..."
+curl -X POST -H "Content-Type:application/rdf+xml" --data-binary @flux.rdf "https://datatourism-bdd.cocktail-culturel.com/blazegraph/namespace/kb/sparql"
+
+echo "Traitement terminé."
