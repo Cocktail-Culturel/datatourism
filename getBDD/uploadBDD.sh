f8acf63b4798cba6a12088657e5f24bc94f10c7e
BIER <cyril.bier42100@gmail.com>
2023-06-21 12:01:35 +0200

    Script de base

--- getBDD/uploadBDD.sh
+++ getBDD/uploadBDD.sh
@@ -3,26 +3,26 @@
 # Lecture du fichier flux.json
 data=$(cat flux.json)
 
-# Extraction des URLs et stockage dans des variables
-Val_de_Loire_Corse_IDF_DOM_TOM=$(echo "$data" | jq -r '."Val-de-Loire+Corse+IDF+DOM/TOM"')
-Nouvelle_Aquitaine=$(echo "$data" | jq -r '."Nouvelle_Aquitaine"')
-Occitanie=$(echo "$data" | jq -r '."Occitanie"')
-Normandie=$(echo "$data" | jq -r '."Normandie"')
-AURA=$(echo "$data" | jq -r '."AURA"')
-Pays_de_la_Loire_PACA=$(echo "$data" | jq -r '."Pays_de_la_Loire+PACA"')
-Haut_De_France=$(echo "$data" | jq -r '."Haut_De_France"')
-Bretagne=$(echo "$data" | jq -r '."Bretagne"')
-Grand_Est=$(echo "$data" | jq -r '."Grand-Est"')
-BFC=$(echo "$data" | jq -r '."BFC"')
+# Déclaration d'un tableau pour stocker les URLs modifiées
+modified_urls=()
 
-# Affichage des variables
-echo "Val_de_Loire_Corse_IDF_DOM_TOM: $Val_de_Loire_Corse_IDF_DOM_TOM"
-echo "Nouvelle_Aquitaine: $Nouvelle_Aquitaine"
-echo "Occitanie: $Occitanie"
-echo "Normandie: $Normandie"
-echo "AURA: $AURA"
-echo "Pays_de_la_Loire_PACA: $Pays_de_la_Loire_PACA"
-echo "Haut_De_France: $Haut_De_France"
-echo "Bretagne: $Bretagne"
-echo "Grand_Est: $Grand_Est"
-echo "BFC: $BFC"
+# Extraction des URLs et ajout dans le tableau
+urls=($(echo "$data" | jq -r '.[]'))
+for url in "${urls[@]}"
+do
+    modified_url="$url"bb3e487f-7b9f-4085-b409-a530cbe2fb90
+    echo "Téléchargement de $modified_url ..."
+    wget "$modified_url" -O flux.gz
+    
+    echo "Décompression de flux.gz ..."
+    gzip -d flux.gz
+    
+    echo "Renommage du fichier décompressé en flux.rdf ..."
+    mv flux flux.rdf
+    
+    echo "Envoi de flux.rdf vers http://localhost:9999/blazegraph/namespace/kb/sparql ..."
+    curl -X POST -H "Content-Type:application/rdf+xml" --data-binary @flux.rdf "http://localhost:9999/blazegraph/namespace/kb/sparql"
+    
+    echo "Traitement terminé pour $modified_url"
+    echo
+done
