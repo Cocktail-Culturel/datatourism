8a101ade6824fd5efe95c3288e125d7022025b83
Mohamed-elg <mohamed.elguermat9@gmail.com>
2023-06-22 13:50:15 +0200

    tentative anti crash

--- API/index.php
+++ API/index.php
@@ -9,7 +9,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->addRoutingMiddleware();
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware = $app->addErrorMiddleware(false, true, false);
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $responseData = array(
